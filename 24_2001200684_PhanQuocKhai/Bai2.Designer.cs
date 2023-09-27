819fa0b6df69bf0b495c8e0df1c066f001e0899b
PhanQuocKhai <hantuongtu2102@gmail.com>
2023-09-27 15:59:17 +0700

    24_2001200684_PhanQuocKhai

---
+++ 24_2001200684_PhanQuocKhai/Bai2.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace _24_2001200684_PhanQuocKhai
+{
+    partial class Bai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttinhnghiem = new System.Windows.Forms.Button();
+            this.btlammoi = new System.Windows.Forms.Button();
+            this.btexit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelx1 = new System.Windows.Forms.Label();
+            this.labelx2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttinhnghiem
+            // 
+            this.bttinhnghiem.Location = new System.Drawing.Point(125, 138);
+            this.bttinhnghiem.Name = "bttinhnghiem";
+            this.bttinhnghiem.Size = new System.Drawing.Size(98, 23);
+            this.bttinhnghiem.TabIndex = 0;
+            this.bttinhnghiem.Text = "Tính Nghiệm";
+            this.bttinhnghiem.UseVisualStyleBackColor = true;
+            this.bttinhnghiem.Click += new System.EventHandler(this.bttinhnghiem_Click);
+            // 
+            // btlammoi
+            // 
+            this.btlammoi.Location = new System.Drawing.Point(305, 138);
+            this.btlammoi.Name = "btlammoi";
+            this.btlammoi.Size = new System.Drawing.Size(100, 23);
+            this.btlammoi.TabIndex = 1;
+            this.btlammoi.Text = "Làm Mới";
+            this.btlammoi.UseVisualStyleBackColor = true;
+            this.btlammoi.Click += new System.EventHandler(this.btnLamMoi_Click);
+            // 
+            // btexit
+            // 
+            this.btexit.Location = new System.Drawing.Point(493, 138);
+            this.btexit.Name = "btexit";
+            this.btexit.Size = new System.Drawing.Size(100, 23);
+            this.btexit.TabIndex = 2;
+            this.btexit.Text = "Thoát";
+            this.btexit.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GIẢI PHƯƠNG TRÌNH BẬC 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X^2 +";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(305, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(437, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X +";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(493, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(623, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "= 0";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(125, 180);
+            this.labelResult.Name = "label4";
+            this.labelResult.Size = new System.Drawing.Size(22, 13);
+            this.labelResult.TabIndex = 9;
+            this.labelResult.Text = "";
+            // 
+            // labelx1
+            // 
+            this.labelx1.AutoSize = true;
+            this.labelx1.Location = new System.Drawing.Point(125, 210);
+            this.labelx1.Name = "labelx1";
+            this.labelx1.Size = new System.Drawing.Size(22, 13);
+            this.labelx1.TabIndex = 9;
+            this.labelx1.Text = "";
+            // 
+            // labelx2
+            // 
+            this.labelx2.AutoSize = true;
+            this.labelx2.Location = new System.Drawing.Point(125, 240);
+            this.labelx2.Name = "labelx2";
+            this.labelx2.Size = new System.Drawing.Size(22, 13);
+            this.labelx2.TabIndex = 9;
+            this.labelx2.Text = "";
+            // 
+            // Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 366);
+            this.Controls.Add(this.labelx1);
+            this.Controls.Add(this.labelx2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btexit);
+            this.Controls.Add(this.btlammoi);
+            this.Controls.Add(this.bttinhnghiem);
+            this.Name = "Bai2";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttinhnghiem;
+        private System.Windows.Forms.Button btlammoi;
+        private System.Windows.Forms.Button btexit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelx1;
+        private System.Windows.Forms.Label labelx2;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ 24_2001200684_PhanQuocKhai/Bai2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24_2001200684_PhanQuocKhai
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "X^2 +";
+            label3.Text = "X +";
+            textBox3.Text = "";
+            bttinhnghiem.Text = "Tính nghiệm";
+            bttinhnghiem.Click += bttinhnghiem_Click;
+        }
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            this.clearAll();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+        private void clearAll() {
+            labelResult.Text = "";
+            labelx1.Text = "";
+            labelx2.Text = "";
+        }
+        private void bttinhnghiem_Click(object sender, EventArgs e)
+        {
+            this.clearAll();
+            double a, b, c;
+            if (double.TryParse(textBox1.Text, out a) && double.TryParse(textBox2.Text, out b) && double.TryParse(textBox3.Text, out c))
+            {
+                double delta = b * b - 4 * a * c;
+                if (delta < 0)
+                {
+                    labelResult.Text = "Phương trình vô nghiệm";
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    labelResult.Text = "Phương trình có nghiệm kép:";
+                    labelx1.Text = "X1 = X2 = " + x;
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    labelResult.Text = "Phương trình có hai nghiệm phân biệt:";
+                    labelx1.Text = "X1 = " + x1;
+                    labelx2.Text = "X2 = " + x2;
+                }
+            }
+            else
+            {
+                labelResult.Text = "Vui lòng nhập hệ số hợp lệ!";
+            }
+        }
+    }
+}
